272b2cfe191b4c63807527fa01ef51e7717c1eee
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-07-15 16:51:41 +0100

    Ensure SceneViewer can see changes

--- src/components/SceneViewer/SceneViewer.js
+++ src/components/SceneViewer/SceneViewer.js
@@ -4,11 +4,24 @@ import React, { Component } from 'react';
 import './scene-viewer.css';
 
 export default class SceneViewer extends Component {
+    state = {
+        locationName: '',
+        scene: undefined,
+    }
+
     render() {
         return (
             <div className="scene-viewer">
-                <h1>Hello</h1>
+                <h1>{ this.state.locationName }</h1>
             </div>
         )
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (!props.location) return state;
+
+        state.locationName = props.location.fields.name;
+
+        return state;
     }
 }
