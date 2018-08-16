2a9b70c362619574d74605cc3f92f27dab8261f6
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-08-16 09:48:03 +0100

    Add close button to modals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+import Dispatcher from '../../store/Dispatcher';
+
+import { MANAGER_VIEWS } from '../../store/ManagerStore';
+
+import '../../css/fontawesome.css';
+import '../../css/solid.css'
 import './modal.css';
 
 export default class Modal extends Component {
@@ -10,9 +16,16 @@ export default class Modal extends Component {
         return (
             <div className="modal">
                 <div className="modal__content">
+                    <button className="modal__close fa fas fa-times fa-lg" label="close" onClick={ this.onDismiss }></button>
                     { this.props.children }
                 </div>
             </div>
         );
+    }
+
+    onDismiss = () => {
+        Dispatcher.dispatch({
+            type: MANAGER_VIEWS.SHOW_SCENE,
+        });
     }
 }
