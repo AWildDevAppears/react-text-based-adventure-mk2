fe826f968b35755d292543e6a9b7167251a47b71
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-09-25 09:45:18 +0100

    Simplify caching

--- src/services/DBService.js
+++ src/services/DBService.js
@@ -1,50 +1,27 @@
-import idb from 'idb';
-
 export default new class DBService {
-    NAME = 'com.awilddevappears.textBasedAdventure';
     dbPromise;
 
-    constructor() {
-        // TODO: Allow us to keep the cache around
-        indexedDB.deleteDatabase(this.NAME);
-
-        this.dbPromise = idb.open(this.NAME, 1, upgradeDB => {
-            upgradeDB.createObjectStore('Zone', { keyPath: 'id' });
-            upgradeDB.createObjectStore('Location', { keyPath: 'id' });
-            upgradeDB.createObjectStore('Scene', { keyPath: 'id' });
-            upgradeDB.createObjectStore('Container', { keyPath: 'id' });
-
-            upgradeDB.createObjectStore('Cache', { keyPath: 'id' });
-        });
+    cache = {
+        Zone: {},
+        Location: {},
+        Scene: {},
+        Container: {},
     }
 
     read(table, key) {
-        return this.dbPromise.then((db) => {
-            return db.transaction(table)
-                .objectStore(table)
-                .get(key);
-        });
+        if (this.cache[table][key] === undefined) return Promise.reject();
+
+        return Promise.resolve(this.cache[table][key]);
     }
 
     update(table, key, value) {
         value.id = key;
+        this.cache[table][key] = value;
 
-        return this.dbPromise.then((db) => {
-            let tx = db.transaction(table, 'readwrite');
-            tx.objectStore(table)
-                .put(value);
-
-            return tx.complete;
-        });
+        return Promise.resolve();
     }
 
     destroy(table, key) {
-        return this.dbPromise.then((db) => {
-            let tx = db.transaction(table, 'readwrite');
-            tx.objectStore(table)
-                .destroy(key);
-
-            return tx.complete;
-        });
+        delete this.cache[table][key]
     }
 }();
